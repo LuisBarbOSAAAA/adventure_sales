3ea1deec026136778e2ce38f71a140cf70632228
luisfmboliveira@gmail.com <luisfmboliveira@gmail.com>
2022-05-24 01:12:38 +0000

    stg_produto estruturado

--- models/staging/stg_produto.sql
+++ models/staging/stg_produto.sql
@@ -1,1 +1,32 @@
-select * from {{source('adventure_dash_vendas','cliente')}}
+with
+    source_produto as (
+        select
+            productid
+            , name
+            , productnumber
+            --, makeflag
+            --, finishedgoodsflag
+            --, color
+            --, safetystocklevel
+            --, reorderpoint
+            --, standardcost
+            --, listprice
+            --, size
+            --, sizeunitmeasurecode
+            --, weightunitmeasurecode
+            --, weight
+            --, daystomanufacture
+            --, productline
+            , class
+            --, style
+            --, productsubcategoryid
+            --, productmodelid
+            --, sellstartdate
+            --, sellenddate
+            --, discontinueddate
+            --, rowguid
+            --, modifieddate
+        from {{source('adventure_dash_vendas','produto')}}
+)
+select *
+from source_produto
