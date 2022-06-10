f98c54988948290d547351dac89d4458fc15e8a7
luisfmboliveira@gmail.com <luisfmboliveira@gmail.com>
2022-06-10 02:53:55 +0000

    Adicionada coluna de valor total na tabela fact_venda

--- models/marts/fact_vendas.sql
+++ models/marts/fact_vendas.sql
@@ -32,7 +32,7 @@ with
     , pedido.status
     , detalhes.orderqty as qtde
     , detalhes.unitprice as preco_unidade
-    , sum (unitprice*orderqty)*(1-unitpricediscount) as total_pedido
+    , (unitprice*orderqty)*(1-unitpricediscount) as total_pedido
     , case 
         when status = 5 then 'enviado'
         when status = 4 then 'rejeitado'
